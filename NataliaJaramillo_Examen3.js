86c7d1dbe5548e4ec1514cc709b2619e033a5b39
natjaram <natjaram@bancolombia.com.co>
2022-05-13 11:49:24 -0500

    Se sube punto 3

---
+++ NataliaJaramillo_Examen3.js
@@ -0,0 +1,23 @@
+var matrizUno=[[3,2,1],[1,2,3],[2,3,1]];
+var matrizDos=[[1,1,2],[2,1,1],[1,2,1]];
+console.log("Arreglo 1: " + matrizUno);
+console.log("Arreglo 2: " + matrizDos);
+
+console.log("Primero * Primero: " + matrizUno[0][0] + "*" + matrizDos[0][0] + "=" + (matrizUno[0][0]*matrizDos[0][0]));
+console.log("Segundo * Segundo: " + matrizUno[0][1] + "*" + matrizDos[0][1] + "=" + (matrizUno[0][1]*matrizDos[0][1]));
+console.log("Tercero * Tercero: " + matrizUno[0][2] + "*" + matrizDos[0][2] + "=" + (matrizUno[0][2]*matrizDos[0][2]));
+console.log("Cuarto * Cuarto: " + matrizUno[1][0] + "*" + matrizDos[1][0] + "=" + (matrizUno[1][0]*matrizDos[1][0]));
+console.log("Quinto * Quinto: " + matrizUno[1][1] + "*" + matrizDos[1][1] + "=" + (matrizUno[1][1]*matrizDos[1][1]));
+console.log("Sexto * Sexto: " + matrizUno[1][2] + "*" + matrizDos[1][2] + "=" + (matrizUno[1][2]*matrizDos[1][2]));
+console.log("Septimo * Septimo: " + matrizUno[2][0] + "*" + matrizDos[2][0] + "=" + (matrizUno[2][0]*matrizDos[2][0]));
+console.log("Octavo * Octavo: " + matrizUno[2][1] + "*" + matrizDos[2][1] + "=" + (matrizUno[2][1]*matrizDos[2][1]));
+console.log("Noveno * Noveno: " + matrizUno[2][2] + "*" + matrizDos[2][2] + "=" + (matrizUno[2][2]*matrizDos[2][2]));
+console.log("Primero + Primero: " + matrizUno[0][0] + "+" + matrizDos[0][0] + "=" + (matrizUno[0][0]+matrizDos[0][0]));
+console.log("Segundo + Segundo: " + matrizUno[0][1] + "+" + matrizDos[0][1] + "=" + (matrizUno[0][1]+matrizDos[0][1]));
+console.log("Tercero + Tercero: " + matrizUno[0][2] + "+" + matrizDos[0][2] + "=" + (matrizUno[0][2]+matrizDos[0][2]));
+console.log("Cuarto + Cuarto: " + matrizUno[1][0] + "+" + matrizDos[1][0] + "=" + (matrizUno[1][0]+matrizDos[1][0]));
+console.log("Quinto + Quinto: " + matrizUno[1][1] + "+" + matrizDos[1][1] + "=" + (matrizUno[1][1]+matrizDos[1][1]));
+console.log("Sexto + Sexto: " + matrizUno[1][2] + "+" + matrizDos[1][2] + "=" + (matrizUno[1][2]+matrizDos[1][2]));
+console.log("Septimo + Septimo: " + matrizUno[2][0] + "+" + matrizDos[2][0] + "=" + (matrizUno[2][0]+matrizDos[2][0]));
+console.log("Octavo + Octavo: " + matrizUno[2][1] + "+" + matrizDos[2][1] + "=" + (matrizUno[2][1]+matrizDos[2][1]));
+console.log("Noveno + Noveno: " + matrizUno[2][2] + "+" + matrizDos[2][2] + "=" + (matrizUno[2][2]+matrizDos[2][2]));
